c747382f0f6c6fc651e4a756f55ce5e5dfbaa40e
Prado <jv_prado@outlook.com>
2025-06-30 20:01:27 -0300

    ajustes gemini

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { i18n } from "astro-i18n-aut/integration";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    i18n({
+      defaultLocale: "pt",
+      locales: {
+        pt: "pt-BR",
+        en: "en-US",
+      },
+    }),
+  ],
 });
